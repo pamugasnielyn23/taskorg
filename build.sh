b51ab27b1271a5d376c0ba0790ce16e70b1c685c
annonymousxxx <maryannlachicalumjod@gmail.com>
2026-01-29 02:33:55 +0800

    Update build.sh with logging and ensure execution permissions

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+echo "Running Build Script..."
 pip install -r requirements.txt
 
+echo "Collecting Static Files..."
 python manage.py collectstatic --no-input
+
+echo "Running Database Migrations..."
 python manage.py migrate
+
+echo "Build Script Complete."
